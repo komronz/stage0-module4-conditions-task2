cd3639bd7328ed3a43a9ef35f32d4d1beba1d778
komronz <pandacoder0820@gmail.com>
2022-07-29 22:30:18 +0500

    Module4Task2

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+        boolean check = from1UpTo1000 > 500;
+        if (check){
+            System.out.println("Tail");
+        }else {
+            System.out.println("Eagle");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        boolean biggest = first>second;
+        if (biggest){
+            System.out.println(first);
+        }else {
+            System.out.println(second);
+        }
     }
 }
